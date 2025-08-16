d56a6bd8b68cbc54d88cf2a10e3aa39474477506
flying-orange <flyingorange9463@gmail.com>
2025-08-16 16:21:09 +0900

    아이템 데이터 선언

---
+++ src/app/services/item.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import Dexie from 'dexie';
+
+
+export interface TakenItem {
+  locationIndex: number;
+  itemIndex: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService extends Dexie {
+  takenItems!: Dexie.Table<TakenItem, [number, number]>;
+
+  constructor() {
+    super('ItemDB');
+    this.version(1).stores({
+      takenItems: '[locationIndex+itemIndex]' // 복합 키 정의
+    });
+  }
+
+
+  /**
+   * 아이템이 획득되었는지 확인
+   */
+  async isItemTaken(locationIndex: number, itemIndex: number): Promise<boolean> {
+    const item = await this.takenItems.get([locationIndex, itemIndex]);
+    return !!item;
+  }
+
+  /**
+   * 아이템 획득 상태를 토글
+   */
+  async toggleItemTaken(locationIndex: number, itemIndex: number): Promise<void> {
+    const isTaken = await this.isItemTaken(locationIndex, itemIndex);
+
+    if (isTaken) {
+      await this.takenItems.delete([locationIndex, itemIndex]);
+    } else {
+      await this.takenItems.put({ locationIndex, itemIndex });
+    }
+  }
+
+}
