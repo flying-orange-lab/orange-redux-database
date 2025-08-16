8682ccc99a9a8abd8f070c82993e785800cd4766
flying-orange <flyingorange9463@gmail.com>
2025-08-17 02:23:49 +0900

    수정 사항 반영

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,20 +4,19 @@ import { DataHandleService } from '../services/data-handle.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less']
+  styleUrls: ['./home.component.less'],
 })
 export class HomeComponent {
   gameVersion: string | null = null;
+  contactFormUrl =
+    'https://docs.google.com/forms/d/e/1FAIpQLSeQQ0w9o5QJ8yBTF40kl3VpqbVqAW1TBJNhuynfS47SO8AhsA/viewform?usp=dialog';
 
-    constructor(
-      public dataHandleService: DataHandleService,
-    ) {}
-    
-    ngOnInit(): void {
-      // 데이터 처리
-      this.dataHandleService.gameVersion$.subscribe(version => {
-        this.gameVersion = version;
-      });
-    }
+  constructor(public dataHandleService: DataHandleService) {}
 
+  ngOnInit(): void {
+    // 데이터 처리
+    this.dataHandleService.gameVersion$.subscribe((version) => {
+      this.gameVersion = version;
+    });
+  }
 }
