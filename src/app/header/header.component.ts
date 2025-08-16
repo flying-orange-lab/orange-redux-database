5e9f9dad188bec8391a7ab2984d6039457d6a0ff
flying-orange <flyingorange9463@gmail.com>
2025-08-16 19:11:32 +0900

    헤더 오류 수정

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,15 +10,24 @@ export class HeaderComponent {
   gameVersion: string | null = null;
   isMenuOpen = false; // 메뉴가 열려있는지 닫혀있는지 상태를 저장하는 변수
 
-  constructor(
-    public dataHandleService: DataHandleService,
-  ) {}
-  
+  constructor(public dataHandleService: DataHandleService) {}
+
   ngOnInit(): void {
     // 데이터 처리
-    this.dataHandleService.gameVersion$.subscribe(version => {
+    this.dataHandleService.gameVersion$.subscribe((version) => {
       this.gameVersion = version;
     });
+  }
+
+  getTitle() {
+    switch (this.gameVersion) {
+      case 'orange_v3':
+        return '모에몬 리덕스 오렌지 에디션 3.24';
+      case 'another_red':
+        return '어나더 레드';
+      default:
+        return '준비중';
+    }
   }
 
   toggleMenu() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataHandleService } from '../services/data-handle.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent {
+  gameVersion: string | null = null;
+
+    constructor(
+      public dataHandleService: DataHandleService,
+    ) {}
+    
+    ngOnInit(): void {
+      // 데이터 처리
+      this.dataHandleService.gameVersion$.subscribe(version => {
+        this.gameVersion = version;
+      });
+    }
 
 }
